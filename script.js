28b8ebbc7bff76c9ebffca5c1d650e5f2c7ae8a1
David Augusto <davidribeiro086@gmail.com>
2022-02-03 13:03:19 -0300

    :sparkles: feat: section banner-sheif

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ productsJson.map((item) => {
 
     let cardItem = document.querySelector('.modal .card').cloneNode(true);
 
+        if(item.promotion == true) {
+            cardItem.querySelector('.card-old-price').innerHTML = `$ ${item.priceOld.toFixed(2)}` ;
+        } else {
+            cardItem.querySelector('.card-old-price').innerHTML = " <br/>";
+        }
         cardItem.querySelector('.card-image').src = item.img;
         cardItem.querySelector('.card-title').innerHTML = item.title;
-        cardItem.querySelector('.card-old-price').innerHTML = `$ ${item.priceOld.toFixed(2)}` ;
         cardItem.querySelector('.card-price').innerHTML = `$ ${item.price.toFixed(2)}`;
 
-    if (item.id <= 6) {
-
-        document.querySelector('.card-area-bought').append(cardItem);
-
-    } else {
-
-        document.querySelector('.card-area-viewed').append(cardItem);
-    }
+        
+        if (item.id <= 6) {
+            document.querySelector('.card-area-bought').append(cardItem);
+        } else {
+            document.querySelector('.card-area-viewed').append(cardItem);
+        }
 });
